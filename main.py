841c23b366cc24e7a5a3f50e28e0386f51e36ce0
Gabriel Castro <ghscastro@hotmail.com>
2022-11-08 14:56:42 -0300

    Print da mensagem de sucesso na busca por algum valor acima de R.000

--- main.py
+++ main.py
@@ -6,6 +6,5 @@ for mes in lista_meses:
     print (mes)
     tabela_vendas = pd.read_excel(f'{mes}.xlsx') # O 'f' antes do texto significa "formatar", ou seja, neste caso, será usado para colocar o nome do arquivo do mês, que está representado pelo "{mes}", antes da extenção .xlsx
     print (tabela_vendas)
-    if (tabela_vendas['Vendas'] >= 55000).any: # O "any" faz com que a busca e a comparação ocorra em cada célula da coluna Vendas, isso garante que o código não fará uma comparação somente na coluna Vendas como um todo, mas sim em cada elemento (célula) dela
-        print()
- 
+    if (tabela_vendas['Vendas'] > 55000).any(): # O "any()" faz com que a busca e a comparação ocorra em cada célula da coluna Vendas, isso garante que o código não fará uma comparação somente na coluna Vendas como um todo, mas sim em cada elemento (célula) dela
+        print(f'No mês de {mes}, alguém bateu a meta!')
