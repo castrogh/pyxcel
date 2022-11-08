10e69c561dddcc53ff12af9e9010f83d261032f3
Gabriel Castro <ghscastro@hotmail.com>
2022-11-08 14:28:55 -0300

    Criação do laço for, para leitura e impressão de cada arquivo .xlsx

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 lista_meses = ['janeiro', 'fevereiro', 'marco', 'abril', 'maio', 'junho']
-tabela_vendas = pd.read_excel ('janeiro.xlsx')
 
-print (tabela_vendas)
+for mes in lista_meses:
+    print (mes)
+    tabela_vendas = pd.read_excel(f'{mes}.xlsx') # O 'f' antes do texto significa "formatar", ou seja, neste caso, será usado para colocar o nome do arquivo do mês, que está representado pelo "{mes}", antes da extenção .xlsx
+    print (tabela_vendas)
+
+ 
